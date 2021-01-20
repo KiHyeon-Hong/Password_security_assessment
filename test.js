884275cf8015a392dc8a1f141188ce788786fb13
KiHyeon-Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2021-01-20 17:59:31 +0900

    levenshtein distance method
    levenshtein distance method

--- test.js
+++ test.js
@@ -1,2 +1,8 @@
-var zxcvbn = require('zxcvbn');
-console.log('hello world!!!');
+const zxcvbn = require('zxcvbn');
+const levenshteinDistance = require('./modules/levenshteinDistance.js');
+
+var orig = 'Th1$1$$tupld';
+var comp = 'chaetopod';
+
+console.log(levenshteinDistance.levenshteinDistance(orig, comp));
+
