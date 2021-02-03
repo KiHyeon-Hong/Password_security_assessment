770ca9b080ba68a1c9dc0b4ea89e5350948ebd98
KiHyeon-Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2021-02-03 16:20:59 +0900

    Update dataNormalization.js

--- dataNormalization.js
+++ dataNormalization.js
@@ -10,6 +10,7 @@ var temp = fs.readFileSync('./files/10k_most_passwords.txt', 'utf-8');
 temp = temp.split('\n');
 
 var data = [];
+var label = [];
 
 for(let i = 0; i < temp.length; i++) {
     let tmp = [];
@@ -68,6 +69,57 @@ for(let i = 0; i < temp.length; i++) {
     else tmp[17] = 0;
 
     data[i] = tmp;
+
+    if(json.sComplexity = 'Very Weak') label[i] = [1, 0];
+    else label[i] = [0, 1];
 }
 
-fs.writeFileSync('./files/dataNormalization.txt', data.toString(), 'utf-8');
+// fs.writeFileSync('./files/dataNormalization.txt', data.toString(), 'utf-8');
+
+
+
+data = tf.tensor(data);
+label = tf.tensor(label);
+
+var X = tf.input({ shape: [18]});
+
+var H = tf.layers.dense({ units: 16, activation:'relu'}).apply(X);
+
+var Y = tf.layers.dense({ units: 2, activation:'sigmoid'}).apply(H);
+
+var model = tf.model({ inputs: X, outputs: Y });
+
+var compileParam = { optimizer: tf.train.adam(), loss: tf.losses.meanSquaredError, metrics:['accuracy'] }
+var compileParam = { optimizer: tf.train.adam(), loss: 'categoricalCrossentropy'}
+
+model.compile(compileParam);
+
+_history = [];
+
+var fitParam = {
+    epochs: 100, 
+    callbacks:{
+    onEpochEnd:
+        function(epoch, logs){
+            console.log('epoch', epoch, logs, 'RMSE=>', Math.sqrt(logs.loss));
+            _history.push(logs);
+        }
+    }
+}
+
+try {
+    model.fit(data, label, fitParam).then(function (result) {
+        // 예측한결과 = new dfd.DataFrame(model.predict(data.tensor));
+        // 예측한결과.print();
+        // label.print();
+        var results = model.predict(data);
+        
+        for(let i = 0; i < results.length; i++){
+            console.log(results.greater(i));
+        }
+    }); 
+}
+catch(e) {
+
+}
+
