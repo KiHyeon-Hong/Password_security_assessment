51c3247735bb116c5f2a6a21ff0aa0e5d884889d
KiHyeon-Hong <ghdrlgus96@gmail.com>
2021-06-08 11:48:41 +0900

    stringMaker.js

--- stringMaker.js
+++ stringMaker.js
@@ -1,19 +1,3 @@
-
-/*
-function randomString(num) {
-	var chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz`~!@#$%^&*()-_/*+={}[]|\\;:'\",.?<>";
-	var string_length = num;
-	var randomstring = '';
-	for (var i=0; i<string_length; i++) {
-		var rnum = Math.floor(Math.random() * chars.length);
-		randomstring += chars.substring(rnum,rnum+1);
-	}
-	return randomstring;
-}
-
-console.log(randomString(Math.floor(Math.random() * 7) + 4));
-*/
-
 /*
 1. 대,소문자, 숫자, 특수 기호 문자 그룹 정의
 2. 문자 그룹 갯수 정하기
@@ -21,21 +5,44 @@ console.log(randomString(Math.floor(Math.random() * 7) + 4));
 4. if 대,소문자 시 사전의 단어를 사용할지? 아니면 랜덤 문자열을 사용하지 정하기
 4-1. 만약 사전의 단어라면 그대로 사용할 것인지? 아니면 몇글자를 바꿀 것인지?
 5. 사전의 단어를 사용하지 않는 문자 그룹들은 몇 글자이며, 해당 문자열 결정
-6. 배밀번호의 길이는 몇자로 결정할지?
+6. 비밀번호의 길이는 몇자로 결정할지?
 7. 100,000개의 데이터 만들기
 */
 
-var smallLetter = "abcdefghijklmnopqrstuvwxyz";
-var capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var number = "0123456789";
-var specialChar = "!@#$%^&*()";
+// characterClass is Class(0: smallLetter, 1: capitalLEtter, 2: number, 3: specialChar)
+// characterNumber is length
+function randomString(characterClass, characterNumber, characterGroups) {
+    /*
+    var smallLetter = "abcdefghijklmnopqrstuvwxyz";
+    var capitalLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var number = "0123456789";
+    var specialChar = "!@#$%^&*()";
+    */
+
+    var chars = ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789", "!@#$%^&*()"];
+   
+	var randomstring = '';
+
+    for(let i = 0; i < characterGroups; i++) {
+        if(characterClass[i] == 4 || characterClass[i] == 5) {
+
+        }
+        else {
+            for(let j = 0; j < characterNumber[i]; j++) {
+                var rnum = Math.floor(Math.random() * chars[characterClass[i]].length);
+                randomstring += chars[characterClass[i]].substring(rnum, rnum + 1);
+            }
+        }
+    }
+
+	return randomstring;
+}
 
 var characterGroups = Math.floor(Math.random() * 10) + 1;
 
 var characterClass = [];
 var characterNumber = [];
-// 0: smallLetter, 1: capitalLEtter, 2: number, 3: specialChar
-// characterNumber is length
+
 for(let count = 0; count < characterGroups; count++) {
     characterClass[count] = Math.floor(Math.random() * 4);
     characterNumber[count] = Math.floor(Math.random() * 10) + 1;
@@ -48,3 +55,17 @@ for(let count = 0; count < characterGroups; count++) {
     }
 }
 
+
+var originalPassword = randomString(characterClass, characterNumber, characterGroups);
+
+/*
+var start = Math.floor(Math.random() * originalPassword.length);
+var passwordLength = Math.floor(Math.random() * chars[characterClass[i]].length);;
+var password = originalPassword.substring(start, start + passwordLength);
+*/
+
+var passwordLength = Math.floor(Math.random() * originalPassword.length) + 1;
+var password = originalPassword.substring(0, passwordLength);
+
+console.log(originalPassword);
+console.log(password);
